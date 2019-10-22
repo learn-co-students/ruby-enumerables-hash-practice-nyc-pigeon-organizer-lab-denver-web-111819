7042375992a9de73c5432a5e076e6c2c13743380
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-22 18:28:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |describers, details|
+    details.each do |features, names|
+      names.each do |name|
+        if pigeon_list[name] == nil 
+        pigeon_list[name] = {}
+        end
+        if pigeon_list[name][describers] == nil 
+        pigeon_list[name][describers] = []
+        end
+      pigeon_list[name][describers] << features.to_s
+      end
+    end
+  end
+    pigeon_list
 end
